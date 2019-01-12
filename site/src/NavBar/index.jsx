b71a8def5343811ceb7b585126787a0e863987f9
drking445 <drking445@gmail.com>
2019-01-12 08:01:27 -0500

    Added Styling and Modified Resume

--- site/src/NavBar/index.jsx
+++ site/src/NavBar/index.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import '../About Me/about.css';
 const NavBar = () => (
     <html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </head>
+    <body>
     <ul className="container-fluid bg-4 text-center">
         <li className={"nav"}><Link to={'/'}>Home </Link></li>
         <li className={"nav"}><Link to='/projects'>Projects</Link></li>
@@ -10,6 +14,7 @@ const NavBar = () => (
         <li className={"nav"}><Link to={'/resume'}>Resume</Link></li>
         <li className={"nav"}><Link to={'/about'}>About Me </Link></li>
     </ul>
+    </body>
     </html>
 );
 
--- site/src/Resume/index.jsx
+++ site/src/Resume/index.jsx
@@ -28,10 +28,10 @@ class Resume extends React.Component{
                 </ul>
             </div>
                     <hr/>
-                <p className="head"><b>Education</b></p>
+                <h3 className="head"><b>Education</b></h3>
                         <p>Morehouse College, Bachelors of Science, Computer Science, May 2018</p>
                         <p>University of Chicago, PhD Machine Learning</p>
-                <p className="head"><b>Technical Skills</b></p>
+                <h3 className="head"><b>Technical Skills</b></h3>
                 <div>
                         <ul className={"resume"}>
                             <li className={"tech"}>Beginner proficiency with React and Angular frameworks</li>
@@ -40,13 +40,29 @@ class Resume extends React.Component{
                         </ul>
                 </div>
                 <div className={"intern"}>
-                    <p><b>Internship Experience</b></p>
+                    <h3><b>Internship Experience</b></h3>
+                    <p><b>Code2040 Fellow PWC Software Engineering Intern: Summer 2017</b></p>
                         <ul className={"resume"}>
-                            <li>Recycling Club</li>
-                            <li>Gardening Club</li>
-                            <li>Book Club</li>
+                            <li className={"tech"}>• Implemented a force touch menu and a deep link for the force touch menu in the existing iOS app so that users can go directly to a certain module within the application without having to enter the application</li>
+                            <li className={"tech"}>• Developed a speech to text Microsoft API into an existing iOS app in order to make linguistic commands and make the user experience in using the application more seamless</li>
+                            <li className={"tech"}>• Created a User Interface for the different navigation pages within the client app using CSS</li>
                         </ul>
                 </div>
+            <div className={"intern"}>
+                <p><b>Code2040 Fellow Goldman Sachs Software Engineering Intern: Summer 2018</b></p>
+                <ul className={"resume"}>
+                    <li className={"tech"}>• Implemented a force touch menu and a deep link for the force touch menu in the existing iOS app so that users can go directly to a certain module within the application without having to enter the application</li>
+                    <li className={"tech"}>• Developed a speech to text Microsoft API into an existing iOS app in order to make linguistic commands and make the user experience in using the application more seamless</li>
+                    <li className={"tech"}>• Created a User Interface for the different navigation pages within the client app using CSS</li>
+                </ul>
+            </div>
+            <div className={"intern"}>
+                <p><b>LookLive Software Engineering Intern: Fall/Spring 2017</b></p>
+                <ul className={"resume"}>
+                    <li className={"tech"}>• Created User Interface pages within the iOS app for all of the clothing modules and search engine for a better user experience using Java and Javascript</li>
+                    <li className={"tech"}>• Developed SQLite database to hold user information and RESTful calls to backend to display profile data upon the user logging into the native iOS application</li>
+                </ul>
+            </div>
             </body>
             </html>
 
