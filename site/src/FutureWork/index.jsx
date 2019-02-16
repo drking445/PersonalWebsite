583e735de6a0dab5280dc0078ae636dc59f62ad9
drking445 <drking445@gmail.com>
2019-02-16 08:16:35 -0500

    Added to Projects Page and Future Work

--- site/src/FutureWork/index.jsx
+++ site/src/FutureWork/index.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import audio from './images/audio.gif';
+
+const img = {
+    width: 150,
+    display: "flex",
+    marginLeft: "auto",
+    marginRight: "auto"
+};
+
 class FutureWork extends React.Component {
 
     render() {
@@ -9,6 +18,10 @@ class FutureWork extends React.Component {
             <title>Future Work</title>
             <h1 className={dish.id}>Here are some of my future project ideas!</h1>
             <h2>Welcome to my HomePage!</h2>
+            <div>
+                <img src={audio} style={img} align="middle"/>
+                <p>I plan on working on a machine learning project that incorporates audio input and from the orchestra in the song on the lyrics, it plays the song from that point in the song. I found this idea a while ago and I hope to implement it in the near future!</p>
+            </div>
             </body>
             </html>
         );
--- site/src/Projects/index.jsx
+++ site/src/Projects/index.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 //import logger from 'logging-library';
 //import FileViewer from 'react-file-viewer';
 import bag from './images/shopping.jpeg';
+import fair from './images/fair.png';
 
 const goat = {
     fontSize: 20,
     fontFamily: "Arial"
+};
+
+const collegeFair = {
+    paddingTop: 25
 };
 
 class Projects extends React.Component{
@@ -18,9 +23,13 @@ class Projects extends React.Component{
             <h1>Projects Page</h1>
             <body>
             Projects Will Be Shown Here:
-            <div>
+            <div style={collegeFair}>
                 <a href={"https://goat-227213.appspot.com/"} ><img src={bag} alt={"Shoppers"}/> </a>
                 <p style={goat}>In this React application I offer a service that I love to do in my free time, reselling! In this application you can buy, sell and lend clothes. This lending functionality is unlike anything on the current market and makes this app special. I hope you use it and love it!</p>
+            </div>
+            <div style={collegeFair}>
+                <a href={"https://spelman-472-2017-1.appspot.com/"} ><img src={fair} alt={"Fair"}/> </a>
+                <p style={goat}>In this web application I developed a way for prospective college students to find schools that fit a number of their interests. This is a favorite app of mine because it solves a problem many of my friends have had and I hope to continue to working on this app to make it better in the future!</p>
             </div>
             </body>
             </html>
